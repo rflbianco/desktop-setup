098387341e323e67155a583e51b1153d4bcb1db4
Rafael Bianco <rafa.bianco@gmail.com>
2016-06-30 13:54:38 -0300

    Move sites definitions.
    Fix redundant path for 'sites' definitions.
    This projec is already meant for desktop, no need to use sites/desktop path.

--- puppet/sites/office.pp
+++ puppet/sites/office.pp
@@ -1,6 +1,6 @@
 include apt
-apt::ppa { 'ppa:libreoffice/ppa': }
 
+apt::ppa { 'ppa:libreoffice/ppa': }
 package { ['libreoffice', 'libreoffice-style-sifr']:
   ensure  => 'latest',
   require => [Apt::Ppa['ppa:libreoffice/ppa']]
--- puppet/sites/themes.gtk.pp
+++ puppet/sites/themes.gtk.pp
@@ -1,11 +1,10 @@
 include ::apt
+
 apt::ppa { 'ppa:noobslab/themes': }
 package { ['arc-theme']:
   ensure  => 'installed',
   require => [Apt::Ppa['ppa:noobslab/themes']]
 }
-
-
 
 apt::ppa { 'ppa:ravefinity-project/ppa': }
 package { ['vivacious-colors-gtk-dark', 'vivacious-colors-gtk-light']:
