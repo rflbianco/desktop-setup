ad2f051edca70c606916ab707ce453dadcb1de9d
Rafael Bianco <rafa.bianco@gmail.com>
2016-06-30 13:47:13 -0300

    Remove various 'classes' moved to 'sites'

--- puppet/manifests/dev.pp
+++
@@ -1,8 +0,0 @@
-class ubuntudesktop::dev {
-  include ubuntudesktop::dev::gradle
-  include ubuntudesktop::dev::mvn
-
-  include ubuntudesktop::dev::git
-  include ubuntudesktop::dev::svn
-  include ubuntudesktop::dev::rabbitvcs
-}
