1c7661ef7e7fbe59e56455f7127d88aeb86c3a4b
Rafael Bianco <rafa.bianco@gmail.com>
2016-07-25 12:28:30 -0300

    Disable 'puppet-agent' startup

--- dependencies.sh
+++ dependencies.sh
@@ -9,6 +9,6 @@ wget https://apt.puppetlabs.com/puppetlabs-release-pc1-xenial.deb
 sudo dpkg -i puppetlabs-release-pc1-xenial.deb
 sudo apt update
 sudo apt install puppet-agent
+sudo update-rc.d puppet disable
 
 sudo gem install librarian-puppet
-
