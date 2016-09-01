bed4265943d0642bc1a4f3172b84287ccb723a20
Rafael Bianco <rafa.bianco@gmail.com>
2016-09-01 14:34:41 -0300

    Add Brazillian Portuguese to spellchecking in LibreOffice

--- puppet/sites/office.pp
+++ puppet/sites/office.pp
@@ -1,7 +1,7 @@
 include apt
 
 apt::ppa { 'ppa:libreoffice/ppa': }
-package { ['libreoffice', 'libreoffice-style-sifr']:
+package { ['libreoffice', 'libreoffice-style-sifr', 'hunspell-pt-br']:
   ensure  => 'latest',
   require => [Apt::Ppa['ppa:libreoffice/ppa']]
 }
