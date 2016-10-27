78191e728b04ded6a5e6c3d83679bfe83e2f78ff
Rafael Bianco <rafa.bianco@gmail.com>
2016-10-27 15:02:01 -0200

    Replace 'virtualbox' from module to PPA
    The puppet module does not use up-to-date repository. It also has not been updated.
    Therefore, the PPA it is a better choice based on up-to-date and convinience.
    The PPA also allows to easily install and keep up-to-date extension pack.

--- puppet/sites/virtualization.pp
+++ puppet/sites/virtualization.pp
@@ -2,4 +2,12 @@
 # include vagrant
 
 # danzilio/virtualbox module
-include virtualbox
+# include virtualbox
+
+include apt
+
+apt::ppa { 'ppa:costamagnagianfranco/virtualbox-ppa': }
+package { ['virtualbox', 'virtualbox-ext-pack']:
+  ensure => "installed",
+  require => [Apt::Ppa['ppa:costamagnagianfranco/virtualbox-ppa']]
+}
