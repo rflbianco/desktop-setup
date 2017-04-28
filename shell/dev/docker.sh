3e21ce7574297e04d78e1fff6beaeea1d47909b5
Rafael Bianco <rafa.bianco@gmail.com>
2017-04-28 12:10:28 -0300

    Add 'yes to all' on docker installation (apt)

--- shell/dev/docker.sh
+++ shell/dev/docker.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 
 sudo apt update
-sudo apt install apt-transport-https ca-certificates
+sudo apt install -y apt-transport-https ca-certificates
 sudo apt-key adv --keyserver hkp://p80.pool.sks-keyservers.net:80 --recv-keys 58118E89F3A912897C070ADBF76221572C52609D
 sudo sh -c 'echo "deb https://apt.dockerproject.org/repo $(echo "$(lsb_release -is)-$(lsb_release -cs)" | tr "[:upper:]" "[:lower:]") main" > /etc/apt/sources.list.d/docker.list'
 sudo apt update
-sudo apt install docker-engine
+sudo apt install -y docker-engine
 sudo update-rc.d docker disable # Disable run on startup
 
 DOCKER_COMPOSE_VERSION=${DOCKER_COMPOSE_VERSION:-"$(curl -s -H "Accept: application/vnd.github.v3+json" -i https://api.github.com/repos/docker/compose/releases/latest | grep -Eo '"tag_name": ".*?"' | grep -Eo '[0-9\.]+')"}
