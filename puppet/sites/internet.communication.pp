37f386ba4f422cd950654455580192168a6163fd
Rafael Bianco <rafa.bianco@gmail.com>
2017-07-05 16:55:37 -0300

    Add Sky, Skype for Business and Lync client

--- puppet/sites/internet.communication.pp
+++ puppet/sites/internet.communication.pp
@@ -18,6 +18,29 @@ package { 'skype':
   require => [Exec['partner-repository-skype']]
 }
 
+
+
+##########################################################
+# Sky - Skype for Business and Lync client
+apt::source { 'sky-ppa':
+  location   => 'https://tel.red/repos/ubuntu',
+  release    => 'xenial',
+  repos      => 'non-free'
+}
+
+apt::key { 'sky-ppa':
+  server => 'keyserver.ubuntu.com',
+  id     => '9454C19A66B920C83DDF696E07C8CCAFCE49F8C5'
+}
+
+package { 'sky':
+  ensure  => 'installed',
+  require => [
+    Apt::Source['sky-ppa'],
+    Apt::Key['sky-ppa']
+  ],
+}
+
 ##########################################################
 # Softphone
 # Disabled in favor of Linphone that provides a CLI option
