5eeca1eadeaf8e44ff123f1b114a252468e63a70
Rafael Bianco <rafa.bianco@gmail.com>
2018-01-29 12:01:45 -0200

    Remove 'powerline' and add default theme to oh-my-zsh

--- shell/ubuntu/oh-my-zsh.sh
+++ shell/ubuntu/oh-my-zsh.sh
@@ -8,9 +8,4 @@ sudo -u $user chsh -s $(which zsh)
 # Install oh-my-zsh framework
 wget https://github.com/robbyrussell/oh-my-zsh/raw/master/tools/install.sh -O - | sudo -u $user zsh
 
-# Install powerlevel9k theme into oh-my-zsh
-sudo -u $user git clone https://github.com/bhilburn/powerlevel9k.git ~/.oh-my-zsh/themes/powerlevel9k
-sudo -u $user sed -i 's,ZSH_THEME=.*,ZSH_THEME="powerlevel9k/powerlevel9k",' ~/.zshrc
-
-# Install Powerline fonts system-wide
-sudo apt install powerline
+sudo -u $user sed -i 's,ZSH_THEME=.*,ZSH_THEME="maran",' ~/.zshrc
