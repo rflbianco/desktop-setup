21660278008ede84a2a0a699c92dbfef537f2814
Rafael Bianco <rafa.bianco@gmail.com>
2018-02-05 12:04:24 -0200

    Add 'rescuetime' dependencies to its installation

--- shell/productivity/rescuetime.sh
+++ shell/productivity/rescuetime.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
+DEPENDENCIES='sqlite3'
+
+sudo apt update && sudo apt install -y ${DEPENDENCIES[@]}
+
 INSTALLER_FILE='/tmp/rescuetime.deb'
 
 wget "https://www.rescuetime.com/installers/rescuetime_current_amd64.deb" -O $INSTALLER_FILE \
 && sudo dpkg --install $INSTALLER_FILE
-
